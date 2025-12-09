61ef74d7afe2a33dd3994c43006c72158e99ccf8
monalper <info@monologed.com>
2025-12-10 00:27:24 +0300

    Update tly.js

--- tly.js
+++ tly.js
@@ -9,12 +9,12 @@ const config = {
         { s: "PEKGY.IS", w: 8.78 },
         { s: "SMRVA.IS", w: 6.81 },
         { s: "DSTKF.IS", w: 5.88 },
-        { s: "XU30.IS", w: 3.76, name: "HMV (Hisse Fon)" },
+        { s: "XU100.IS", w: 3.76, name: "HMV (Hisse Fon)" },
         { s: "TURSG.IS", w: 1.69 },
         { s: "HEDEF.IS", w: 1.61 },
         { s: "ENSRI.IS", w: 0.40 },
         { s: "GRTHO.IS", w: 0.26 },
-        { s: "XU30.IS", w: 0.12, name: "T3B (Hisse Fon)" },
+        { s: "XU100.IS", w: 0.12, name: "T3B (Hisse Fon)" },
         { s: "IZFAS.IS", w: 0.05 },
         { s: "ADESE.IS", w: 0.03 }
     ],
@@ -26,4 +26,5 @@ const config = {
 document.addEventListener('DOMContentLoaded', () => {
     new FundCalculator(config);
 });
+
 
